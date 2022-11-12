4ceb8cc18bbb7735de962dc0564599b43bc78443
thejas <thejaskala308@gmail.com>
2022-11-12 15:14:11 +0530

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require("./routes/userRoutes")
-const cors = require('cors')
+// const cors = require('cors')
 require('dotenv').config();
 const app = express();
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use('/api/auth',userRoutes)
 
